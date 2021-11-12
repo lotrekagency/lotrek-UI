ad044351c8694520ee84c673b2a0525717d0e42c
andrea morosi <andreamorosi94@gmail.com>
2021-11-12 15:31:33 +0100

    Fixing github pages generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/lotrek-UI/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
