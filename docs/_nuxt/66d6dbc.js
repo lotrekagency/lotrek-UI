bb150ab3928ef255c8af3794bde42113ef91fd28
andrea morosi <andreamorosi94@gmail.com>
2021-11-12 15:23:39 +0100

    Added docs generated template

---
+++ docs/_nuxt/66d6dbc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{167:function(n,o,w){w(168),n.exports=w(169)}},[[167,13,2,14]]]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -59,5 +59,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  generate: {
+    dir: 'docs'
   }
 }
