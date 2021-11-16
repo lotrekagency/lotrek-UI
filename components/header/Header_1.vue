1342418a1e2acfd5fa97214ac469530db0a8e3a1
andrea morosi <andreamorosi94@gmail.com>
2021-11-16 16:18:24 +0100

    New header block

---
+++ components/header/Header_1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="headr">
+    <div class="headr__inner">
+
+      <a href="" class="headr__logo">
+        <img src="../../static/assets/images/placeholder.svg" alt="" class="headr__logo__img">
+      </a><!--/headr__logo-->
+
+      <div class="headr__hmb">
+        <span class="headr__hmb__line"></span>
+        <span class="headr__hmb__line"></span>
+        <span class="headr__hmb__line"></span>
+      </div><!--/headr__hmb-->
+
+      <nav class="headr__core">
+        <a href="" class="headr__core__link">Link 1</a>
+        <a href="" class="headr__core__link">Link 2</a>
+        <a href="" class="headr__core__link">Link 3</a>
+        <a href="" class="headr__core__link">Link 4</a>
+      </nav><!--/headr__core-->
+
+    </div><!--/headr__inner-->
+  </div><!--/headr-->
+</template>
