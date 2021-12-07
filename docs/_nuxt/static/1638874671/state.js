b3e4f29c0d7756805e2fc54977f7d5e9c49ebf80
andrea morosi <andreamorosi94@gmail.com>
2021-12-07 11:58:32 +0100

    Docs generation

---
+++ docs/_nuxt/static/1638874671/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flotrek-UI\u002F_nuxt\u002Fstatic\u002F1638874671",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Flotrek-UI\u002F",assetsPath:"\u002Flotrek-UI\u002F_nuxt\u002F",cdnURL:a}}}}(null));
